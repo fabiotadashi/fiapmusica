3969ed1d3021968bd517f400f47bc9e1e6a7e8eb
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2021-03-16 21:05:50 -0300

    user repository

--- src/main/java/br/com/fiap/fiapmusica/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/fiapmusica/repository/UsuarioRepository.java
@@ -4,4 +4,7 @@ import br.com.fiap.fiapmusica.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Usuario findFirstByNome(String nome);
+
 }
